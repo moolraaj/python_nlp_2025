7982c2e531e78a9c10280db21e31d85be8b5c565
neosharma995 <neosharma995@gmail.com>
2025-04-22 12:28:18 +0530

    updated new code

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
  
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["https://your-app.onrender.com"],
+  allow_origins=["http://localhost:4500"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
