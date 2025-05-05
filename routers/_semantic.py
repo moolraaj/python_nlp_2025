30844affb7e2e53d9676b826042cebeaf48af6ac
moolraaj <116911577+moolraaj@users.noreply.github.com>
2025-05-05 17:14:51 +0530

    updated new code

--- routers/_semantic.py
+++ routers/_semantic.py
@@ -16,6 +16,7 @@ nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('universal_tagset')
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
